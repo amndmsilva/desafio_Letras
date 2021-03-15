b0f48347013454f272845b40ed908053f4ffd283
Amanda Santos <amndmsilva@gmail.com>
2021-03-15 20:43:16 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var randomNumber = document.querySelectorAll('display h1');
+
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+//const grid;
+// speed = 256
+
+// var selectRandomNumber = function (){
+//     for(var rn = 0; rn < randomNumber.length; rn++) {
+//         var random = Math.round(Math.random() * 10);
+
+
+//         console.log(random);
+//     };
+// }
+
+
+const reset = function () {
+
+}
